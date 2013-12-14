a583bfdec7c09ed10a00440cc48346474113df12
blueplus <blueplus@blueplussMacBook-Pro.local>
2013-12-14 23:46:25 +0800

    firstClass

---
+++ MyFirstIOSApp/MyTestClass.h
@@ -0,0 +1,13 @@
+//
+//  MyTestClass.h
+//  MyFirstIOSApp
+//
+//  Created by blueplus on 13-12-14.
+//  Copyright (c) 2013年 blueplus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyTestClass : NSObject
+
+@end
